1891bf8489057debffb420efcef982b676822968
MachineMuse <claire.alexandria@gmail.com>
2013-10-16 01:28:41 -0600

    added skeleton of GreatSpirit structure added summoning staff reworked items in anticipation of IDless system in 1.7

--- src/net/machinemuse/anima/item/AnimaItemBase.scala
+++ src/net/machinemuse/anima/item/AnimaItemBase.scala
@@ -3,13 +3,18 @@ package net.machinemuse.anima.item
 import net.minecraft.item.Item
 import net.machinemuse.anima.AnimaTab
 import net.machinemuse.numina.item.NuminaItemBase
+import net.minecraft.client.renderer.texture.IconRegister
+import net.minecraft.util.Icon
 
 /**
  * Author: MachineMuse (Claire Semple)
  * Created: 11:46 PM, 10/15/13
  */
-abstract class AnimaItemBase(val unlocalizedName: String) extends Item(ItemIDManager.getID(unlocalizedName)) with AnimaItem {
-
+abstract class AnimaItemBase(val name: String) extends Item(ItemIDManager.getID(name)) with AnimaItem {
+  var icon:Icon=null
+  def iconRegistration(register:IconRegister) {
+    icon = register.registerIcon("anima:" + name)
+  }
 }
 
 trait AnimaItem extends NuminaItemBase {
